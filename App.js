e940441ea4f3a97f9acc829a8fa574babcdddf50
Muhammad Ali <alihere786@gmail.com>
2026-05-13 18:45:03 +0500

    chore: app running with updated Stack import

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import RootNavigator from './src/navigations/RootNavigator.js';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from 'react-native';
+import colors from './src/constants/colors.js';
+
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView
+        style={{ flex: 1 }}
+        edges={['top', 'bottom']}
+        backgroundColor={colors.mainBackground}
+      >
+        <StatusBar hidden={false} barStyle={'dark-content'} translucent />
+        <RootNavigator />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+};
+
+export default App;
